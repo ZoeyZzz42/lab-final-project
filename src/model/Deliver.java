d6e80784b8b8a5dc27fce9f2a43d77d21803d7aa
ZoeyZzz42 <zhang.ziyi7@northeastern.edu>
2023-12-08 19:24:55 -0500

    解决合并冲突，保留文件更改

---
+++ src/model/Deliver.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author jq
+ */
+public class Deliver {
+    private String deliverId;
+    private String productName;
+
+    /**
+     * @return the deliverId
+     */
+    public String getDeliverId() {
+        return deliverId;
+    }
+
+    /**
+     * @param deliverId the deliverId to set
+     */
+    public void setDeliverId(String deliverId) {
+        this.deliverId = deliverId;
+    }
+
+    /**
+     * @return the productName
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * @param productName the productName to set
+     */
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    
+    
+}
